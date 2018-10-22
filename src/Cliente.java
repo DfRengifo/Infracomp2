b759722e638d3f38d16d18710c5fa3678014d26c
Aflosada <af.losada@uniandes.edu.co>
2018-10-22 15:50:37 -0500

    aaaaa odio mi bida

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,45 @@
 
 public class Cliente 
 {
+    private String mensaje;
+    private static Servidor servidor;
+
+
+    public Cliente( Servidor pServidor, String pMensaje) {
+
+        this.mensaje = pMensaje;
+        this.servidor = pServidor;
+    }
+
+    @Override
+    public void run()
+    {
+
+            Mensaje m = mensajes.get(i);
+       almacenar(m);
+            System.out.println("El cliente almaceno el mensaje " + i);
+
+        buff.numClientes= buff.numClientes -1;
+        System.out.println("el numero de clientes es " +buff.numClientes);
+    }
+
+    public synchronized void  almacenar(Mensaje mensaje)
+    {
+        while (buff.tamano <= 0)
+        {
+            yield();
+        }
+        buff.almacenar(mensaje);
+        synchronized(mensaje)
+        {
+            try {
+
+                mensaje.wait();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
